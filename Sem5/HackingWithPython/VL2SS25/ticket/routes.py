68f93210807687b1f36cfd5f0e6834628c021e6d
Fabian Brzesina <fbrzesina2004@gmail.com>
2025-04-02 15:48:44 +0200

    update

--- Sem5/HackingWithPython/VL2SS25/ticket/routes.py
+++ Sem5/HackingWithPython/VL2SS25/ticket/routes.py
@@ -1,5 +1,6 @@
-from ticket import app
-from flask import render_template, request
+from ticket import app, db
+from flask import render_template, request, redirect, url_for
+from sqlalchemy import text
 
 @app.route('/')
 def home_page():
@@ -7,10 +8,16 @@ def home_page():
 
 @app.route('/tickets')
 def ticket_page():
-    items = [{"id": 1, "priority": 2, "username": "Marc", "title": "something broke"},
-             {"id": 2, "priority": 1, "username": "Barc", "title": "nothing worky"},
-             {"id": 3, "priority": 3, "username": "Quarc", "title": "idk man.."}]
-    
+    #items = [{"id": 1, "priority": 2, "username": "Marc", "title": "something broke"},
+    #         {"id": 2, "priority": 1, "username": "Barc", "title": "nothing worky"},
+    #         {"id": 3, "priority": 3, "username": "Quarc", "title": "idk man.."}]
+    query_stmt = f"SELECT * FROM ticket"
+    result = db.session.execute(text(query_stmt))
+    items = result.fetchall()
+
+    print(items)
+
+
     return render_template("tickets.html", items=items)
 
 @app.route('/login', methods=["GET", "POST"])
@@ -20,6 +27,18 @@ def login_page():
         username = request.form.get("Username")
         password = request.form.get("Password")
         print(f"Username: {username}, Password: {password}")
+
+        query_stmt = f"SELECT * FROM benutzer WHERE benutzername = '{username}' AND passwort = '{password}'"
+        result = db.session.execute(text(query_stmt))
+        items = result.fetchall()
+        print(items)
+
+        if len(items) > 0:
+            print("User exists")
+            resp = redirect(url_for('ticket_page'))
+            resp.set_cookie("benutzerId", str(items[0][0]))
+            return resp
+
     return render_template("login.html")
 
 @app.route('/register', methods=["GET", "POST"])
