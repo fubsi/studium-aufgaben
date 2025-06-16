fixes MLP BackProp

Sem5/ILS_Praktikum/versuch3/V3A1/MLP_Backprop.py
@@ -17,21 +17,23 @@ def softmax(a):
     e_a = np.exp(a - np.max(a))  # subtract maximum potential such that maximal exponent is 1 (for numerical stability)
     return e_a / e_a.sum()       # return softmax function value
 
-def forwardPropagateActivity(x,W1,W2,flagBiasUnit=1): 
+
+
+def forwardPropagateActivity(x,W1,W2,flagBiasUnit=1):
     """
-    Propagate neuronale activity through the network in forward direction from input layer to output layer   
-    :param x: Input vector (may be extended with a bias unit) 
+    Propagate neuronale activity through the network in forward direction from input layer to output layer
+    :param x: Input vector (may be extended with a bias unit)
     :param W1: Weight matrix for synaptic layer 1 (connecting the input layer to the hidden layer)
     :param W2: Weight matrix for synaptic layer 2 (connecting the hidden layer to the output layer)
     :param flagBiasUnit: If >0 then add a bias unit to the hidden layer
     :returns z_1: Firing rates of the neurons in the hidden layer (=layer 1)
-    :returns z_2: Firing rates of the neurons in the output layer (=layer 2); it is z2=y:=output activity 
-    """    
-    a_1 = np.dot(W1, x)                                  # REPLACE DUMMY CODE: compute dendritic potentials of hidden layer a_1
-    z_1 = softmax(a_1)                     # REPLACE DUMMY CODE: compute activity z_1 of hidden layer 1 
+    :returns z_2: Firing rates of the neurons in the output layer (=layer 2); it is z2=y:=output activity
+    """
+    a_1 = np.dot(W1, x)                         # compute dendritic potentials of hidden layer a_1
+    z_1 = np.tanh(a_1)                          #  compute activity z_1 of hidden layer 1
     if flagBiasUnit>0: z_1=np.append(z_1,[1.0]) # add bias unit (with constant activity 1) to hidden layer ?
-    a_2 = np.dot(W2, z_1)                                  # REPLACE DUMMY CODE: compute dendritic potentials of output layer a_2 
-    z_2 = softmax(a_2)                # REPLACE DUMMY CODE: compute softmax activations for output layer 
+    a_2 = np.dot(W2, z_1)                       #  compute dendritic potentials of output layer a_2
+    z_2 = softmax(a_2)                          #  compute softmax activations for output layer
     return z_1, z_2;                            # return activities in layers 1 and 2; z_2 corresponds to outputs y
 
 def backPropagateErrors(z_1,z_2,t,W1,W2,flagBiasUnit=1): # backpropagate error signals delta_L
@@ -47,32 +49,35 @@ def backPropagateErrors(z_1,z_2,t,W1,W2,flagBiasUnit=1): # backpropagate error s
     :returns delta_2: Error signals for (output) layer 2
     """
     y=z_2                                     # layer 2 is output layer
-    delta_2=y-t                 # REPLACE DUMMY CODE: Initializing error signals in output layer 2  
-    alpha_1=np.dot(W2.T,delta_2)                              # REPLACE DUMMY CODE: compute error potentials in hidden layer 1 by backpropagating errors delta_2
-    h_prime=1.0-np.multiply(z_1,z_1)                              # REPLACE DUMMY CODE: factor (1-z_1.*z_1) is h'(a) for tanh sigmoid function 
-    delta_1=np.multiply(h_prime,alpha_1)                 # REPLACE DUMMY CODE: compute error signals in hidden layer 1
-    if flagBiasUnit>0: delta_1 = delta_1[:-1] # remove last error signal corresponding to the bias unit ?  
+    delta_2= y - t                            # Initializing error signals in output layer 2
+    alpha_1= np.dot(W2.T, delta_2)            # compute error potentials in hidden layer 1 by backpropagating errors delta_2
+    h_prime= 1 - z_1 **2                      # factor (1-z_1.*z_1) is h'(a) for tanh sigmoid function
+    delta_1= h_prime * alpha_1                #  compute error signals in hidden layer 1
+    if flagBiasUnit>0: delta_1 = delta_1[:-1] # remove last error signal corresponding to the bias unit ?
     return delta_1, delta_2                   # return error signals for each layer
+
 
 def doLearningStep(W1,W2,xn,tn,eta,lmbda_by_N=0,flagBiasUnit=1): # do one backpropagation learning step...
     """
-    Do one backpropagation learning step for one input vector xn with corresponding traget vector tn 
+    Do one backpropagation learning step for one input vector xn with corresponding traget vector tn
     :param W1: Weight matrix for synaptic layer 1 (connecting the input layer to the hidden layer)
     :param W2: Weight matrix for synaptic layer 2 (connecting the hidden layer to the output layer)
-    :param xn: Input vector (may be extended with a bias unit) 
+    :param xn: Input vector (may be extended with a bias unit)
     :param tn: true target vector (=output label) for input vector xn as obtained from training data
     :param eta: learning rate (determines how far to go into neg-gradient direction)
     :param lmbda_by_N: regularization coefficient lambda divided by N (=lambda/N)
-    :param flagBiasUnit: If >0 then add a bias unit to the hidden layer 
-    :returns W1,W2: updated weight matrices 
+    :param flagBiasUnit: If >0 then add a bias unit to the hidden layer
+    :returns W1,W2: updated weight matrices
     """
     z_1    ,z_2    =forwardPropagateActivity(xn,W1,W2,flagBiasUnit);    # forward propagation of activity according to input vector xn
     delta_1,delta_2=backPropagateErrors(z_1,z_2,tn,W1,W2,flagBiasUnit); # get error signals by backpropagation
-    nablaED_1 = np.outer(delta_1,xn)                            # REPLACE DUMMY CODE: gradient of data error function for first layer
-    nablaED_2 = np.outer(delta_2,z_1)                             # REPLACE DUMMY CODE: gradient of data error function for second layer
-    W1=eta * (nablaED_1 + lmbda_by_N * W1)                                        # REPLACE DUMMY CODE: update weights for first layer with "weight decay" regularization
-    W2=eta * (nablaED_2 + lmbda_by_N * W2)                                        # REPLACE DUMMY CODE: update weights for second layer with "weight decay" regularization
-    return W1,W2                                 # return new weights
+    nablaED_1 = np.outer(delta_1, np.concatenate((xn, [1])))[:, :-1]    #  gradient of data error function for first layer
+    nablaED_2 = np.outer(delta_2, z_1)                                  # gradient of data error function for second layer
+    W1 -= eta * (nablaED_1 + lmbda_by_N * W1)
+    W2 -= eta * (nablaED_2 + lmbda_by_N * W2)
+    return W1,W2                                                         # return new weights
+
+
 
 def getError(W1,W2,X,T,lmbda=0,flagBiasUnit=1): # compute total crossentropy error function over whole data set (X,T) for MLP with weight layers W1,W2
     """

Sem5/ILS_Praktikum/versuch3/V3A1/MLP_Backprop_GridSearch_CrossVal.py
@@ -155,9 +155,10 @@ if __name__ == '__main__':
     print("T=",T)
 
     # (ii) Train MLP
-    M=[14]                                # number of hidden units
-    eta=[1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7]                            # learning rate
-    lmbda=[0.9, 0.75, 0.5, 0.25, 0.1, 0.01, 0.001]                            # regularization coefficient
+    S=3
+    M=range(1,15)                                # number of hidden units
+    eta=[1e-3, 1e-4, 1e-5]                            # learning rate
+    lmbda=[0, 0.1]                            # regularization coefficient
     bestM=None
     bestEta=None
     bestW1=None
@@ -182,23 +183,39 @@ if __name__ == '__main__':
                 if -1 in epochs4plot: plotDecisionSurface(W1,W2,gridX,gridY,X1,X2,contlevels,-1,flagBiasUnit)
                 lastLocalError=None
                 starttime = time.time()  # start time for learning
-                for epoch in range(nEpochs):       # loop over learning epochs
-                    errc = 0                       # initialize classification errors with zero
-                    for n in range(N):             # loop over all training data        
-                        xn=X[n,:]                  # n-th data vector
-                        tn=T[n,:]                  # n-th target value
-                        yn=forwardPropagateActivity(xn,W1,W2)[1]  # test training vector xn
-                        yhat,that=2,2              # initialize class labels 
-                        if(tn[0]>=tn[1]): that=1   # actual class label
-                        if(yn[0]>=yn[1]): yhat=1   # predicted class by MLP 
-                        if(yhat!=that): errc=errc+1                                   # count classification error
-                        W1,W2=doLearningStep(W1,W2,xn,tn,e,l/N) # do one backprop learning update of weights
-                    E=getError(W1,W2,X,T)
-                    if lastLocalError is None or (lastLocalError>E and np.absolute(E-lastLocalError)>1e-3): # if error function has decreased
-                        lastLocalError=E
-                        print("Epoch ",epoch,": Error E=",E,end="\r")
-                    else:
-                        break # stop learning if error function does not decrease anymore
+                perm = np.random.permutation(N)
+                idxS = [range(i*N//S,(i+1)*N//S) for i in range(S)] # indexes for dividing data set into S parts of equal size
+                valMatCp = np.zeros((K,K))                   # initialize confusion probability matrix Cp[i,j]=pr[true class i and predicted class j] 
+                valErr = 0
+                bestValErr = 0
+                for idxVal in idxS:                                 # loop over all S validation data sets
+                    # (i) generate training and testing data sets and train classifier        
+                    if S>1: idxTrain = [i for i in range(N) if i not in idxVal]               # remaining indices (not in idxVal) are training data
+                    else  : idxTrain = idxVal                                                 # if S==1 use entire data set for training and validation
+                    for epoch in range(nEpochs):       # loop over learning epochs
+                        errc = 0                       # initialize classification errors with zero       
+                        xn=X[perm[idxTrain]]                  # n-th data vector
+                        tn=T[perm[idxTrain]]                  # n-th target value
+                        for idx,_ in enumerate(xn):  # loop over all training indexes
+                            yn=forwardPropagateActivity(xn[idx],W1,W2)[1]  # test training vector xn
+                            yhat,that=2,2              # initialize class labels 
+                            if(tn[idx][0]>=tn[idx][1]): that=1   # actual class label
+                            if(yn[0]>=yn[1]): yhat=1   # predicted class by MLP 
+                            if(yhat!=that): errc=errc+1                                   # count classification error
+                            W1,W2=doLearningStep(W1,W2,xn[idx],tn[idx],e,l/N) # do one backprop learning update of weights
+                            for i in range(len(idxVal)):  # loop over all validation indexes
+                                y_hat = np.argmax(forwardPropagateActivity(X[perm[i]], W1, W2)[1])     # predicted class of i-th input vector from validation set 
+                                t_true = np.argmax(T[perm[i]])                     # corresponding true class label
+                                valMatCp[t_true,y_hat]+=1                  # increase component of confusion matrix 
+                                if(y_hat!=t_true): valErr+=1               # increase counter of errors
+                            valMatCp=(1.0/N)*valMatCp    # divide by data number to get confusion probability matrix
+                            valErr=valErr/float(N)       # divide by data number to get error probability 
+                        E=getError(W1,W2,X,T)
+                        if lastLocalError is None or (lastLocalError>E and np.absolute(E-lastLocalError)>1e-3): # if error function has decreased
+                            lastLocalError=E
+                            print(f"Epoch {epoch}: Err={E}, ValErr={valErr}",end="\r")
+                        else:
+                            break # stop learning if error function does not decrease anymore
 
                 endtime = time.time()
                 if epoch == nEpochs-1: # only plot decision surface for last epoch
@@ -209,7 +226,7 @@ if __name__ == '__main__':
                     # found better MLP with less classification errors or same errors but smaller error function
                     lastError=E
                     lastErrc=errc
-                    print("Found better MLP with m=",m," and eta=",e," and lmbda=",l," with error E=",E, " and classification errors = ", errc, " after ", epoch, " epochs")
+                    print("Found better MLP with m=",m," and eta=",e," and lmbda=",l," with error E=",E," valErr=",valErr, " and classification errors = ", errc, " after ", epoch, " epochs")
                     bestM=m
                     bestEta=e
                     bestW1=W1
@@ -217,9 +234,10 @@ if __name__ == '__main__':
                     bestLmbda=l
                     bestCEpochs=epoch
                     bestTime=endtime-starttime
+                    bestValErr=valErr
                     # plotDecisionSurface(W1,W2,gridX,gridY,X1,X2,contlevels,epoch,flagBiasUnit)
                     # plt.show()
-    print("Best MLP with m=",bestM," and eta=",bestEta," and lmbda=",bestLmbda," has error E=", lastError, " and classification errors = ", lastErrc, " after ", bestCEpochs, " epochs", " and took ", bestTime, " seconds to train.")
+    print("Best MLP with m=",bestM," and eta=",bestEta," and lmbda=",bestLmbda," has error E=", lastError," valErr=",bestValErr, " and classification errors = ", lastErrc, " after ", bestCEpochs, " epochs", " and took ", bestTime, " seconds to train.")
     print("Final weights:")
     print("W1=",bestW1)
     print("W2=",bestW2)